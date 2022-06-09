e6aac7a9fbffca759b0412d2d292ab678db03943
engle212 <engle.212@buckeyemail.osu.edu>
2022-06-08 21:30:55 -0400

    Completed
    Project now functions!

--- src/renderer/Display.java
+++ src/renderer/Display.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 
 import renderer.point.MyPoint;
 import renderer.shapes.MyPolygon;
+import renderer.shapes.Tetrahedron;
 
 // John Engle
 
@@ -22,6 +23,8 @@ public class Display extends Canvas implements Runnable {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     private static boolean running = false;
+
+    private Tetrahedron tetra;
 
     public Display() {
         this.frame = new JFrame();
@@ -67,6 +70,8 @@ public class Display extends Canvas implements Runnable {
         double delta = 0;
         int frames = 0;
 
+        this.init();
+
         while (running) {
             long now = System.nanoTime();
             delta += (now - lastTime) / ns;
@@ -74,9 +79,9 @@ public class Display extends Canvas implements Runnable {
             while (delta >= 1) {
                 this.update();
                 delta--;
+                this.render();
+                frames++;
             }
-            this.render();
-            frames++;
 
             if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
@@ -85,6 +90,26 @@ public class Display extends Canvas implements Runnable {
             }
         }
         this.stop();
+
+    }
+
+    private void init() {
+        int s = 100;
+        MyPoint p1 = new MyPoint(s / 2, -s / 2, -s / 2);
+        MyPoint p2 = new MyPoint(s / 2, s / 2, -s / 2);
+        MyPoint p3 = new MyPoint(s / 2, s / 2, s / 2);
+        MyPoint p4 = new MyPoint(s / 2, -s / 2, s / 2);
+        MyPoint p5 = new MyPoint(-s / 2, -s / 2, -s / 2);
+        MyPoint p6 = new MyPoint(-s / 2, s / 2, -s / 2);
+        MyPoint p7 = new MyPoint(-s / 2, s / 2, s / 2);
+        MyPoint p8 = new MyPoint(-s / 2, -s / 2, s / 2);
+
+        this.tetra = new Tetrahedron(new MyPolygon(Color.RED, p1, p2, p3, p4),
+                new MyPolygon(Color.BLUE, p5, p6, p7, p8),
+                new MyPolygon(Color.WHITE, p1, p2, p6, p5),
+                new MyPolygon(Color.YELLOW, p1, p5, p8, p4),
+                new MyPolygon(Color.GREEN, p2, p6, p7, p3),
+                new MyPolygon(Color.ORANGE, p4, p3, p7, p8));
 
     }
 
@@ -100,16 +125,14 @@ public class Display extends Canvas implements Runnable {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, WIDTH * 2, HEIGHT * 2);
 
-        MyPolygon poly = new MyPolygon(new MyPoint(0, 100, 0),
-                new MyPoint(0, 0, 0), new MyPoint(0, 50, 50));
-        poly.render(g);
+        this.tetra.render(g);
 
         g.dispose();
         bs.show();
     }
 
     private void update() {
-
+        this.tetra.rotate(true, 1, 1, 1);
     }
 
 }
--- src/renderer/shapes/MyPolygon.java
+++ src/renderer/shapes/MyPolygon.java
@@ -4,15 +4,31 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import renderer.point.MyPoint;
 import renderer.point.PointConverter;
 
 public class MyPolygon {
 
+    private Color color;
     private MyPoint[] points;
 
+    public MyPolygon(Color color, MyPoint... points) {
+        this.color = color;
+        this.points = new MyPoint[points.length];
+        for (int i = 0; i < points.length; i++) {
+            MyPoint p = points[i];
+            this.points[i] = new MyPoint(p.x, p.y, p.z);
+        }
+
+    }
+
     public MyPolygon(MyPoint... points) {
+        this.color = Color.WHITE;
         this.points = new MyPoint[points.length];
         for (int i = 0; i < points.length; i++) {
             MyPoint p = points[i];
@@ -27,7 +43,50 @@ public class MyPolygon {
             Point p = PointConverter.convertPoint(this.points[i]);
             poly.addPoint(p.x, p.y);
         }
-        g.setColor(Color.RED);
+        g.setColor(this.color);
         g.fillPolygon(poly);
     }
+
+    public void rotate(boolean CW, double xDegrees, double yDegrees,
+            double zDegrees) {
+        for (MyPoint p : this.points) {
+            PointConverter.rotateAxisX(p, CW, xDegrees);
+            PointConverter.rotateAxisY(p, CW, yDegrees);
+            PointConverter.rotateAxisZ(p, CW, zDegrees);
+        }
+    }
+
+    public double getAverageX() {
+        double sum = 0;
+        for (MyPoint p : this.points) {
+            sum += p.x;
+        }
+        return sum / this.points.length;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public static MyPolygon[] sortPolygons(MyPolygon[] polygons) {
+        List<MyPolygon> polygonsList = new ArrayList<MyPolygon>();
+
+        for (MyPolygon poly : polygons) {
+            polygonsList.add(poly);
+        }
+
+        Collections.sort(polygonsList, new Comparator<MyPolygon>() {
+            @Override
+            public int compare(MyPolygon p1, MyPolygon p2) {
+                return p2.getAverageX() - p1.getAverageX() < 0 ? 1 : -1;
+            }
+        });
+
+        for (int i = 0; i < polygons.length; i++) {
+            polygons[i] = polygonsList.get(i);
+        }
+
+        return polygons;
+    }
+
 }
